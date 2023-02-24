6fdcf21a0ad8b2f06870140da11d1ef2872035df
Marcus Young <myoung34@my.apsu.edu>
2023-02-23 22:42:52 -0600

    update the things

--- custom.sh
+++ custom.sh
@@ -26,7 +26,6 @@ function kubepug() {
 . $HOME/.asdf/completions/asdf.bash
 
 # tfenv
-export PATH="$HOME/.tfenv/bin:$PATH"
 
 function upsearch () {
   found=$(test / == "$PWD" && return || test -e "$1" && echo "$PWD" && return || cd .. && upsearch "$1")
@@ -39,8 +38,9 @@ ack() {
   rg $@
 }
 
-alias k="kubectl"
-export PATH="$HOME/.krew/bin:$PATH"
+alias k="/home/myoung/.asdf/shims/kubectl"
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+export PATH="$HOME/.tfenv/bin:$PATH"
 
 alias rlp="source ~/.custom.sh"
 
@@ -58,8 +58,27 @@ ssh-add >/dev/null 2>&1
 ssh-add ~/.ssh/gh_reddit >/dev/null 2>&1 #for github.snooguts
 
 function phage() {
-  [[ ! -d $HOME/.age ]] && mkdir $HOME/.age
-  [[ ! -f $HOME/.age/key ]] && age-plugin-yubikey -l --serial 11087061 --slot 1 | grep -v '^#' >$HOME/.age/key
+  mkdir -p $HOME/.age || :
+  [[ ! -f $HOME/.age/recipient ]] && age-plugin-yubikey -l --serial 11087061 --slot 1 | grep -v '^#' >$HOME/.age/recipient
+  [[ ! -f $HOME/.age/identity ]] && age-plugin-yubikey -i >$HOME/.age/identity
+  filename=$(basename -- "$1")
+  extension="${filename##*.}"
+
+  if [[ ${extension} == "age" ]]; then 
+    filename="${filename%.*}"
+    echo Decrypting ${filename}.age to ${filename}
+    age -d -i ~/.age/identity -o ${filename} ${filename}.age
+  else
+    echo Encrypting ${filename} to ${filename}.age
+    age -R "${HOME}/.age/recipient" -o ${filename}.age ${filename}
+  fi
+}
+
+function funs() {
+  NAMESPACE=$1
+  kubectl proxy &
+  kubectl get namespace $NAMESPACE -o json |jq '.spec = {"finalizers":[]}' >temp.json
+  curl -k -H "Content-Type: application/json" -X PUT --data-binary @temp.json 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
 }
 
 [[ $(uname -r) == *"microsoft"* ]] && export BROWSER="powershell.exe /C start"
