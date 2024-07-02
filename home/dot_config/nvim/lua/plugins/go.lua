2e222cf4329d978b1761363c13fef1fe25abec74
Marcus Young <myoung34@my.apsu.edu>
2024-07-02 21:05:45 +0000

    Add nvim, refactor file names

---
+++ home/dot_config/nvim/lua/plugins/go.lua
@@ -0,0 +1,16 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.go" },
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {},
+    ft = { "go", "gomod" },
+    build = function()
+      require("go.install").update_all_sync()
+    end,
+  },
+}
